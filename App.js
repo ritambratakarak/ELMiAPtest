ed131157f4f2d0a5e6d65c3357c624a27d2ad5ab
Ritambrata Karak <ritambrata@elearnmarkets.com>
2022-06-01 18:59:31 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+import React,{useEffect} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './App/Utils/Navigation'
+import { Provider } from 'react-redux';
+import reduxStore from './App/Redux/reduxConfig';
+
+const store = reduxStore()
+
+const App = () => {
+
+
+
+  return (
+    <Provider store={store}>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
