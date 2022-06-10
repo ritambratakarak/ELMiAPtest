759e6184a0e0e18a30fc4bb7835bbf2648b19e4a
Ritambrata Karak <ritambrata@elearnmarkets.com>
2022-06-10 16:15:19 +0530

    function update

--- App/Screens/Subscription/index.js
+++ App/Screens/Subscription/index.js
@@ -31,6 +31,11 @@ const Subscription = props => {
   const [purchased, setPurchased] = useState(false);
 
   useEffect(() => {
+    checkSubscription()
+  }, []);
+
+
+  const checkSubscription = async () =>{
     IAP.initConnection()
       .catch(() => {
         console.log('error connecting to store...');
@@ -41,23 +46,33 @@ const Subscription = props => {
             console.log('error finding items');
           })
           .then(res => {
-            alert(JSON.stringify(res));
             setProducts(res);
           });
+        await IAP.getAvailablePurchases()
+        .then(async res => {
+          try {
+            const receipt = res;
+            alert(JSON.stringify(receipt));
+          } catch (error) {}
+        });
         IAP.getPurchaseHistory()
           .catch(() => {})
           .then(async res => {
             try {
               const receipt = res[res.length - 1].transactionReceipt;
               if (receipt) {
-                alert(JSON.stringify(receipt));
                 setPurchased(true);
                 await AsyncStorage.setItem('purchase', JSON.stringify(receipt))
               }
             } catch (error) {}
           });
       });
-  }, []);
+  }
+
+  const subscriptionPress =(productId) =>{
+    IAP.requestSubscription(productId);
+    checkSubscription()
+  }
 
   return (
     <>
@@ -65,14 +80,22 @@ const Subscription = props => {
         {products.length > 0 ? (
           <View style={styles.repeatContainer}>
             {products.map(p => (
-              <Button
-                key={p['productId']}
-                title={`Purchase ${p['title']}`}
-                onPress={() => {
-                  console.log(p['productId']);
-                  IAP.requestSubscription(p['productId']);
-                }}
-              />
+              <TouchableOpacity style={{backgroundColor:"blue", width: '90%', height:200, marginVertical:15, justifyContent:"center", alignItems:"center", }} onPress={()=> subscriptionPress(p['productId'])}>
+                <Text style={{color:"#fff", fontSize:15}}>{`${p['title']}`}</Text>
+                <Text style={{color:"#fff", fontSize:15}}>{`Price: ${p['originalPrice']}`}</Text>
+                <View style={{backgroundColor:"#fff", borderRadius:20, alignSelf:"center", width:30, height:30}}>
+                  <Text>Buy</Text>
+                </View>
+              </TouchableOpacity>
+              // onPress={()=> subscriptionPress(p['productId'])}
+              // <Button
+              //   key={p['productId']}
+              //   title={`Purchase ${p['title']}`}
+              //   onPress={() => {
+              //     console.log(p['productId']);
+              //     IAP.requestSubscription(p['productId']);
+              //   }}
+              // />
             ))}
           </View>
         ) : (
