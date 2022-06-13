e5f0eacf9caa3557cb7e1058577d0898ad63167c
Ritambrata Karak <ritambrata@elearnmarkets.com>
2022-06-13 17:58:45 +0530

    changes

--- App/Screens/Subscription/index.js
+++ App/Screens/Subscription/index.js
@@ -1,5 +1,12 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {StyleSheet, Image, View, Text, TouchableOpacity, Linking} from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 import {HEIGHT, GAP, COLORS, WIDTH, FONT} from '../../Utils/constants';
 import IAP from 'react-native-iap';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -34,7 +41,6 @@ const Subscription = props => {
         console.log('error connecting to store...');
       })
       .then(async () => {
-
         IAP.getSubscriptions(itemSubs)
           .catch(() => {
             console.log('error finding items');
@@ -43,12 +49,12 @@ const Subscription = props => {
             setProducts(res);
           });
         IAP.getPurchaseHistory()
-        .catch(() => {})
-        .then(async res => {
-          try {
-            const receipt = res[res.length - 1].transactionReceipt;
-            if (receipt) {
-                setPurchaseddata(receipt)
+          .catch(() => {})
+          .then(async res => {
+            try {
+              const receipt = res[res.length - 1].transactionReceipt;
+              if (receipt) {
+                setPurchaseddata(receipt);
                 alert(JSON.stringify(receipt));
                 await AsyncStorage.setItem('purchase', JSON.stringify(receipt));
                 setPurchased(true);
@@ -63,22 +69,78 @@ const Subscription = props => {
     checkSubscription();
   };
 
-  const Unsubscribe =()=>{
-    Linking.openURL('https://play.google.com/store/account/subscriptions?package=com.elmiaptest.application&sku='+ purchaseddata?.productId)
-  }
+  const Unsubscribe = () => {
+    Linking.openURL(
+      'https://play.google.com/store/account/subscriptions?package=com.elmiaptest.application&sku=' +
+        purchaseddata?.productId,
+    );
+  };
 
   if (purchased) {
     return (
       <View style={styles.container}>
-        <View style={{justifyContent: 'center', alignSelf: 'center', flex: 1, alignItems:"center", }}>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignSelf: 'center',
+            flex: 1,
+            alignItems: 'center',
+          }}>
           <Image
             source={require('../../Assets/tick.png')}
             style={{height: 100, width: 100}}
           />
-          <Text style={styles.title}>{purchaseddata?.productId == "elm_monthly_test_autorenew_subscription" ? "Monthly Subscription is active" : purchaseddata?.productId == "elm_quarter_test_autorenew_subscription" ? "Quaterly Subscription is active" : purchaseddata?.productId == "elm_yearly_test_autorenew_subscription" ? "Yearly Subscription is active" : ""}</Text>
+          <Text style={styles.title}>
+            {purchaseddata?.productId ==
+            'elm_monthly_test_autorenew_subscription'
+              ? 'Monthly Subscription is active'
+              : purchaseddata?.productId ==
+                'elm_quarter_test_autorenew_subscription'
+              ? 'Quaterly Subscription is active'
+              : purchaseddata?.productId ==
+                'elm_yearly_test_autorenew_subscription'
+              ? 'Yearly Subscription is active'
+              : ''}
+          </Text>
           <Text style={styles.title}>You are already subscribe to app</Text>
-          
-          <TouchableOpacity style={{marginVertical:10}} onPress={Unsubscribe}>
+          {products
+            .filter(item => item !== purchaseddata?.productId)
+            .map((p) => {
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#1D458A',
+                  width: '100%',
+                  height: 200,
+                  marginVertical: 15,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 15,
+                }}
+                key={p['productId']}
+                onPress={() => subscriptionPress(p['productId'])}>
+                <Text
+                  style={{color: '#fff', fontSize: 20}}>{`${p['title']}`}</Text>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 18,
+                  }}>{`Price: ${p['originalPrice']}`}</Text>
+                <View
+                  style={{
+                    backgroundColor: '#fff',
+                    alignSelf: 'center',
+                    width: 80,
+                    height: 40,
+                    alignItems: 'center',
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    margin: 10,
+                  }}>
+                  <Text style={{fontSize: 18, color: '#000'}}>Buy</Text>
+                </View>
+              </TouchableOpacity>
+            })}
+          <TouchableOpacity style={{marginVertical: 10}} onPress={Unsubscribe}>
             <Text style={styles.title}>Unsubscribe</Text>
           </TouchableOpacity>
         </View>
@@ -121,7 +183,7 @@ const Subscription = props => {
                   justifyContent: 'center',
                   margin: 10,
                 }}>
-                <Text style={{fontSize: 18, color:"#000"}}>Buy</Text>
+                <Text style={{fontSize: 18, color: '#000'}}>Buy</Text>
               </View>
             </TouchableOpacity>
             // onPress={()=> subscriptionPress(p['productId'])}
@@ -155,12 +217,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginBottom: HEIGHT * 0.04,
   },
-  title:{
-    fontSize:15,
-    fontWeight:'600',
-    paddingVertical:10,
-    color:"#000"
-  }
+  title: {
+    fontSize: 15,
+    fontWeight: '600',
+    paddingVertical: 10,
+    color: '#000',
+  },
 });
 
 export default Subscription;
