65f06bedbdc28dd17e135d778a10e7ef2487ad2d
Koushal <koushal@elearnmarkets.com>
2022-06-13 11:08:13 +0530

    push

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import 'react-native-gesture-handler';
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './App/Utils/Navigation'
 import { Provider } from 'react-redux';
 import reduxStore from './App/Redux/reduxConfig';
+import codePush from "react-native-code-push";
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_START };
+
 
 const store = reduxStore()
 
 const App = () => {
-
-
+  useEffect(() => {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+    });
+  }, []);
 
   return (
     <Provider store={store}>
-        <NavigationContainer>
-          <Navigation />
-        </NavigationContainer>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
     </Provider>
   );
 };
 
-export default App;
+export default codePush(codePushOptions)(App);
--- android/app/src/main/java/com/elmipatest/MainApplication.java
+++ android/app/src/main/java/com/elmipatest/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.soloader.SoLoader;
 import com.elmipatest.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,6 +23,11 @@ public class MainApplication extends Application implements ReactApplication {
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
         }
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+
 
         @Override
         protected List<ReactPackage> getPackages() {
