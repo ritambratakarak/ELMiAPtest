daceeafe07b61791a39e1fb5b71eefcda923cd06
Santanu <santanu@kredentacademy.com>
2022-07-26 10:23:54 +0530

    test for download

--- App/Utils/Routes.js
+++ App/Utils/Routes.js
@@ -25,7 +25,7 @@ const Routes = () => {
   return (
     <NavigationContainer>
       {/* {user ? <Navigation /> : <AuthStack />} */}
-      <AuthStack />
+      <Navigation />
 
     </NavigationContainer>
   );
